7b21c68c0821409835ddd60c18e60a9ef0144240
mbritzke <matheusbritzke@gmail.com>
2023-01-20 23:41:12 -0300

    Estrutura da aplicação criada

---
+++ gym-service/src/test/java/com/org/eldorado/gymservice/GymServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.eldorado.gymservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
